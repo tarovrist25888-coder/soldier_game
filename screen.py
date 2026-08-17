3465ef6864b5e26028b89427bed80e242f5be80e
tarovrist25888-coder <tarovrist25888@gmail.com>
2026-08-17 16:14:37 +0300

    Add files via upload

--- screen.py
+++ screen.py
@@ -17,16 +17,17 @@ screen = pygame.display.set_mode(
 pygame.display.set_caption('SOLDIER GAME')
 
 soldier = pygame.image.load("bin\\soldier.png").convert()
-soldier = pygame.transform.scale(soldier,(TILE_SIZE*2,TILE_SIZE*4))
+soldier = pygame.transform.scale(soldier, (TILE_SIZE * 2, TILE_SIZE * 4))
 
 flag = pygame.image.load("bin\\flag.png").convert()
-flag = pygame.transform.scale(flag,(TILE_SIZE*3,TILE_SIZE*4))
+flag = pygame.transform.scale(flag, (TILE_SIZE * 3, TILE_SIZE * 4))
 
 grass = pygame.image.load("bin\\grass.png").convert()
-grass = pygame.transform.scale(grass, (3*TILE_SIZE, 2*TILE_SIZE))
+grass = pygame.transform.scale(grass, (3 * TILE_SIZE, 2 * TILE_SIZE))
 
 mine = pygame.image.load("bin\\mine.png").convert()
-mine = pygame.transform.scale(mine,(TILE_SIZE*3,TILE_SIZE))
+mine = pygame.transform.scale(mine, (TILE_SIZE * 3, TILE_SIZE))
+
 
 def fill_screen():
     screen.fill(consts.LIGHT_GREEN)
@@ -45,32 +46,35 @@ def random_grass():
 
     return grass_positions
 
+
 def draw_bushes(grass_positions):
     for i in grass_positions:
         screen.blit(grass, i)
+
 
 def visualize_board(board, mines):
     rects = []
     for row in range(len(board)):
         for col in range(len(board[row])):
-            x = (col*consts.TILE_SIZE)
-            y = (row*consts.TILE_SIZE)
+            x = (col * consts.TILE_SIZE)
+            y = (row * consts.TILE_SIZE)
             draw.rect(screen, consts.GREEN, (x, y, consts.TILE_SIZE, consts.TILE_SIZE), 1)
             rects.append((x, y))
-    
+
     for i in range(len(mines)):
-        screen.blit(mine, (mines[i][0]*TILE_SIZE, mines[i][1]*TILE_SIZE))
+        screen.blit(mine, (mines[i][0] * TILE_SIZE, mines[i][1] * TILE_SIZE))
+
 
 def draw_soldier():
     soldier_pos = create_soldier()
     screen.blit(soldier, soldier_pos)
 
+
 def draw_flag():
-    flag_pos = (47*TILE_SIZE, 21*TILE_SIZE)
+    flag_pos = (47 * TILE_SIZE, 21 * TILE_SIZE)
     screen.blit(flag, flag_pos)
 
-
-
-
-
-
+def draw_message(message):
+    font = pygame.font.SysFont("Calibri", 5)
+    text_img = font.render(message, True, "black")
+    screen.blit(text_img, (10, 10))
