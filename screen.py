cb72380aa4cb7c179156cff59a107b0ee4e5bfe3
sarichazon <sarichazon2008@gmail.com>
2026-08-17 16:25:28 +0300

    Add files via upload

--- screen.py
+++ screen.py
@@ -6,32 +6,32 @@ from pygame.constants import QUIT
 import consts
 from consts import TILE_SIZE
 import soldier
-from game_field import get_flag_indexes
-from soldier import create_soldier
 
-pygame.init()
 
-screen = pygame.display.set_mode(
+
+
+
+window = pygame.display.set_mode(
     (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
-
 pygame.display.set_caption('SOLDIER GAME')
 
-soldier = pygame.image.load("bin\\soldier.png").convert()
-soldier = pygame.transform.scale(soldier, (TILE_SIZE * 2, TILE_SIZE * 4))
-
+soldier_img = pygame.image.load("bin\\soldier.png").convert()
+soldier_resized=pygame.transform.scale(soldier_img,(TILE_SIZE*2,TILE_SIZE*4))
 flag = pygame.image.load("bin\\flag.png").convert()
-flag = pygame.transform.scale(flag, (TILE_SIZE * 3, TILE_SIZE * 4))
-
+flag_resized=pygame.transform.scale(flag,(TILE_SIZE*4,TILE_SIZE*3))
 grass = pygame.image.load("bin\\grass.png").convert()
-grass = pygame.transform.scale(grass, (3 * TILE_SIZE, 2 * TILE_SIZE))
-
+grass_resized=pygame.transform.scale(grass,(TILE_SIZE,TILE_SIZE))
 mine = pygame.image.load("bin\\mine.png").convert()
-mine = pygame.transform.scale(mine, (TILE_SIZE * 3, TILE_SIZE))
+mine_resized=pygame.transform.scale(mine,(TILE_SIZE*3,TILE_SIZE))
 
-
-def fill_screen():
-    screen.fill(consts.LIGHT_GREEN)
+def create_screen():
+    screen = pygame.display.set_mode(
+        (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
     return screen
+def fill_screen(screen):
+    screen.fill(consts.GREEN)
+
+
 
 
 def random_grass():
@@ -45,36 +45,50 @@ def random_grass():
         grass_positions.append((x, y))
 
     return grass_positions
-
-
-def draw_bushes(grass_positions):
+def draw_bushes(screen,grass_positions):
     for i in grass_positions:
-        screen.blit(grass, i)
+        screen.blit(grass_resized,i)
 
 
-def visualize_board(board, mines):
-    rects = []
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            x = (col * consts.TILE_SIZE)
-            y = (row * consts.TILE_SIZE)
-            draw.rect(screen, consts.GREEN, (x, y, consts.TILE_SIZE, consts.TILE_SIZE), 1)
-            rects.append((x, y))
+def draw_grid(screen):
+    # Horizontal lines
+    for row in range(consts.FIELD_ROWS + 1):
 
-    for i in range(len(mines)):
-        screen.blit(mine, (mines[i][0] * TILE_SIZE, mines[i][1] * TILE_SIZE))
+        y = row * TILE_SIZE
 
+        pygame.draw.line(
+            screen,
+            consts.BLACK,
+            (0, y),
+            (consts.WINDOW_WIDTH, y)
+        )
+     # Vertical lines
+    for col in range(consts.FIELD_COLS + 1):
 
-def draw_soldier():
-    soldier_pos = create_soldier()
+        x = col * TILE_SIZE
+
+        pygame.draw.line(
+            screen,
+            consts.BLACK,
+            (x, 0),
+            (x, consts.WINDOW_HEIGHT)
+        )
+
+def draw_mines(screen,mines):
+    for mine in mines():
+        for row,col in mine:
+            x=row*TILE_SIZE
+            y=col*TILE_SIZE
+            screen.blit(mine_resized,(x,y))
+def draw_soldier(screen):
+    soldier_pos = soldier.create_soldier()
     screen.blit(soldier, soldier_pos)
 
 
-def draw_flag():
+def draw_flag(screen):
     flag_pos = (47 * TILE_SIZE, 21 * TILE_SIZE)
     screen.blit(flag, flag_pos)
-
 def draw_message(message):
     font = pygame.font.SysFont("Calibri", 5)
     text_img = font.render(message, True, "black")
-    screen.blit(text_img, (10, 10))
+    window.blit(text_img, (10, 10))
